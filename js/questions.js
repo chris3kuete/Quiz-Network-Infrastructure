edb02a709226640264fe92a6a0436eb1ff7f84e9
chris3kuete <chriskuete@yahoo.fr>
2024-12-28 23:42:09 -0800

    1

---
+++ js/questions.js
@@ -0,0 +1,223 @@
+let questions = [
+    {
+        numb: 1,
+        question: "A technician from your ISP has arrived to help you troubleshoot a weak WAN connection, to what location do you take her ?",
+        answer: "Entrance facility",
+        options: [
+            "IDF",
+            "Work area",
+            "CEO'S office",
+            "Entrance facility"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A transceiver was recently damaged by a lightning strike during a storm. How might you decide whether the ISP is responsible for replacing this device, or whether your company must foot the bill ?",
+        answer: "Look at whether the device is located on the ISPs side of the demarc",
+        options: [
+            "Look at whether the device is located on the ISPs side of the demarc",
+            "Look at the manufacturer information on the device's label",
+            "Look at purchase record for the device to determine when it was acquired",
+            "Look at what kinds of cables are connected to the device"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following devices are you likely to find in the MDF ?",
+        answer: "Routers,Switches,KVM switch",
+        options: [
+            "Routers,Switches,KVM switch",
+            "Laptop,APs",
+            "Network printer,workstation",
+            "Dock station,cameras"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which device converts signals from a campus's analog phone equipment into IP data that can travel over the internet ?",
+        answer: "VoIP gateway",
+        options: [
+            "VoIP PBX",
+            "VoIP endpoint",
+            "VoIP gateway",
+            "VoIP switch"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What type of diagram shows a graphical representation of a network's wired infrastructure?",
+        answer: "Wiring schematic",
+        options: [
+            "Rack diagram",
+            "Wiring schematic",
+            "Network map",
+            "Network topology"
+        ]
+    },
+    {
+        numb: 6,
+        question: "If you are shopping for a rack switch, what component on the switch tells you it can be mounted to a rack ?",
+        answer: "Rack ears",
+        options: [
+            "AC adapter",
+            "Rack ears",
+            "Padded feet",
+            "Large fans"
+        ]
+    },
+    {
+        numb: 7,
+        question: "You need to connect a new network printer to a nearby wall jack. What kind of cable should you use ?",
+        answer: "Patch cable",
+        options: [
+            "Fiber optic cable",
+            "Patch cable",
+            "Backbone cable",
+            "Plenum-rated cable"
+        ]
+    },
+    {
+        numb: 8,
+        question: "You've decided to run an Nmap scan on your network. What app could you open to perform this task?",
+        answer: "Zenmap,Command prompt,PowerShell",
+        options: [
+            "google,OneDrive,AWS Console",
+            "Microsoft edge,google chrome",
+            "Zenmap,Command prompt,PowerShell",
+            "OKTA,Zscaler,vsCode"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of these is considered a secure place to store a list of documented network passwords ?",
+        answer: "A password manager",
+        options: [
+            "The CEO's smartphone",
+            "A sticky note under the keyboard",
+            "A password manager",
+            "The MDF"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the first step of inventory management ?",
+        answer: "List all components on the network",
+        options: [
+            "Interview users",
+            "Backup network data",
+            "List an administrative account's username and password for each device on a network",
+            "List all components on the network"
+        ]
+    },
+    {
+        numb: 11,
+        question: " There is only one __ per network, but there can be many __ connecting internal portion of the network",
+        answer: "MDF,IDF",
+        options: [
+            "MDF,IDF",
+            "ARP,ATB",
+            "BRP,BDP",
+            "SMP,UDP"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Why is it important to use a structured cabling standard when installing and managing cabling systems ?",
+        answer: "To maximize performance and minimize upkeep",
+        options: [
+            "To use cables efficiently",
+            "To maximize performance and minimize upkeep",
+            "To improve productivity.",
+            "To have a resilient system."
+        ]
+    },
+    {
+        numb: 13,
+        question: "Why is it important to use plenum-rated cabling in the area above the ceiling tile ?",
+        answer: "Because that area of the house is usually hot so you need a cable suitable for those temperatures",
+        options: [
+            "Because they are very light",
+            "Because of it's max length",
+            "Because that area of the house is usually hot so you need a cable suitable for those temperatures",
+            "Because of their weight"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What is the unit of measurement that defines the space available in a rack? ",
+        answer: "Rack Units",
+        options: [
+            "Rack feet",
+            "Rack Centimeters",
+            "Rack Meters",
+            "Rack Units"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Why is it important to minimize cable clutter in a rack?",
+        answer: "Helps prevent airflow blockages and heat buildup",
+        options: [
+            "To secure the rack",
+            "Helps prevent airflow blockages and heat buildup",
+            "To be able to see the cables labels",
+            "To make the inside of the rack more visible"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What are some elements that are typically included in network diagrams ?",
+        answer: "Switch, router, firewall, server, workstation",
+        options: [
+            "transceivers, crimper, tone generator, packet sniffer",
+            "Switch, router, firewall, server, workstation",
+            "cables, mouse, keyboard, ipad",
+            "hardrive, headset, camera, phone"
+        ]
+    },
+    {
+        numb: 17,
+        question: "How tall are standard racks?",
+        answer: "42U tall",
+        options: [
+            "42U tall",
+            "43U tall",
+            "44U tall",
+            "45U tall"
+        ]
+    },
+    {
+        numb: 18,
+        question: "How can you go about gathering the information needed to assemble a thorough operations manual ?",
+        answer: "Take some time to brainstorm all the elements you want to include in the manual and consider how you will keep each elements in the manual updated",
+        options: [
+            "Do some researchs on google",
+            "Ask help from colleagues",
+            "Take some time to brainstorm all the elements you want to include in the manual and consider how you will keep each elements in the manual updated",
+            "Get an experienced network engineer to do it for you"
+        ]
+    },
+    {
+        numb: 19,
+        question: "For what time period should you schedule a network change?",
+        answer: "Maintenance window",
+        options: [
+            "Office hours",
+            "Holiday",
+            "Weekend",
+            "Maintenance window"
+        ]
+    },
+    {
+        numb: 20,
+        question: " In a large organization, how do you typically request permission to perform a network change ?",
+        answer: "Submit a change request document,understand and follow the approval process",
+        options: [
+            "Submit a change request document,understand and follow the approval process",
+            "Ask to the IT manager during a meeting",
+            "Talk to the CEO of the organization",
+            "Make an announcement on the organization website"
+        ]
+    },
+
+]
